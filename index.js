1f84dd9bad419e56f251610421b82dc9f880c372
codetheorem <hrishikeshagarwalv@gmail.com>
2020-06-21 14:26:10 +0530

    added guildonly feat

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const fetch = require('node-fetch');
 const querystring = require('querystring');
 const config = require('./config.json');
 const axios = require('axios');
+// const { guildOnly } = require('./commands/addorg');
 const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 var servername = null;
-
 
 bot.once('ready', () => {
 	console.log('Ready!');
@@ -29,7 +29,7 @@ for (const file of commandFiles) {
 bot.on('message', async message => {
 
 	
-	 servername=message.guild.name;
+	 
 
     if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
@@ -37,9 +37,34 @@ const args = message.content.slice(config.prefix.length).split(/ +/);
 const command = args.shift().toLowerCase();
 
 
+console.log(command.guildOnly);
+
+if (command==='addorg' && message.channel.type !== 'text') {
+	return message.reply("I can't execute that command inside DMs!");
+}
+
+if(message.channel.type==='text')
+{servername=message.guild.name;}
+
+
+
+
+
+// try{
+// 	servername=message.guild.name;
+// } catch(error){
+// 	 return message.reply("I can't execute that command inside DMs!");
+// }
+
+
+
+
+
 	console.log(message.content);
 
-    if (!bot.commands.has(command)) return;
+    if (!bot.commands.has(command)) { return message.reply(
+		'Invalid command. Try `git help` to learn more about available commands'
+	);}
 
 	try {
 		bot.commands.get(command).execute(message, args);

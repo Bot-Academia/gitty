3acd042e6a55c596c1043740aad2b492eeac243a
codetheorem <hrishikeshagarwalv@gmail.com>
2020-06-22 20:23:59 +0530

    minor changes

--- commands/help.js
+++ commands/help.js
@@ -11,15 +11,15 @@ module.exports = {
       .addFields(
         {name:'server info',value: 'git serverinfo',inline: true},
         {name:'member info',value: 'git memberinfo',inline: true},
-        { name:'Get org info', value: "git org",inline:true},
-        {name:'github info',value: 'git user',inline:true},
+        { name:'Get org info', value: "git org <orgname>",inline:true},
+        {name:'github info',value: 'git user <username>',inline:true},
         {name:'quote for life',value: 'git quote',inline:true},
-        {name:'Issue list of a github repo',value: 'git issues',inline:true},
-        {name:'PR list of a github repo',value: 'git pulls',inline:true},
-        {name:'Add a default organization',value: 'git addorg',inline:true},
+        {name:'Issue list of a github repo',value: 'git issues <reponame>',inline:true},
+        {name:'PR list of a github repo',value: 'git pulls <reponame>',inline:true},
+        {name:'Add a default organization',value: 'git addorg <orgname>',inline:true},
       )
       .setFooter('Check [git about] to know about me 🤖');
-    message.author.send(embed);
+    message.channel.send(embed);
 
   },
 };

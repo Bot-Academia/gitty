eec7967469c9d27eeefa3d8515ebf74f353531ac
Hrishikesh Agarwal <hrishikeshagarwalv@gmail.com>
2020-06-23 11:51:11 +0530

    Update user.js

--- commands/user.js
+++ commands/user.js
@@ -6,8 +6,8 @@ module.exports = {
   description: "Tells github info",
   async execute(message, args) {
     var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    const trim = (str, max) =>
-      str.length > max ? `${str.slice(0, max - 3)}...` : str;
+    // const trim = (str, max) =>
+    //   str.length > max ? `${str.slice(0, max - 3)}...` : str;
 
     if (!args.length) {
       return message.channel.send("You need to supply a search term!");
@@ -24,11 +24,11 @@ module.exports = {
       .setColor("#" + randomColor)
       .setTitle("Github User")
       .addFields(
-        { name: "Name", value: trim(list.name, 1024) },
-        { name: "Location", value: trim(list.location, 1024) },
-        { name: "Bio", value: trim(list.bio, 1024) },
-        { name: "Public Repos", value: trim(list.public_repos, 1024) },
-        { name: "URL", value: trim(list.html_url, 1024) }
+        { name: "Name", value: list.name },
+        { name: "Location", value: list.location },
+        { name: "Bio", value: list.bio },
+        { name: "Public Repos", value: list.public_repos },
+        { name: "URL", value: list.html_url }
       );
     message.channel.send(embed);
   },

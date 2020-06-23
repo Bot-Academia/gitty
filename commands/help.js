47e218d7b5f18b21fbf1067328407ad0a7c7c304
codetheorem <hrishikeshagarwalv@gmail.com>
2020-06-23 19:24:25 +0530

    updated help

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports = {
-  name: "help",
+  name: "help",     
   description: "shows command list",
   execute(message, args) {
     const embed = new Discord.MessageEmbed()
@@ -9,14 +9,14 @@ module.exports = {
       .setTitle("Gitty command list")
       .setDescription('Gitty is a discord bot to help you with Open source')
       .addFields(
-        {name:'server info',value: 'git serverinfo',inline: true},
-        {name:'member info',value: 'git memberinfo',inline: true},
-        {name:'Get org info', value: "git org <orgname>",inline:true},
-        {name:'github info',value: 'git user <username>',inline:true},
-        {name:'quote for life',value: 'git quote',inline:true},
-        {name:'Issue list of a github repo',value: 'git issues <reponame>',inline:true},
-        {name:'PR list of a github repo',value: 'git pulls <reponame>',inline:true},
-        {name:'Add a default organization',value: 'git addorg <orgname>',inline:true},
+        {name:'server info',value: '`git serverinfo`',inline: true},
+        {name:'member info',value: '`git memberinfo`',inline: true},
+        {name:'Login your org', value: "`git addorg <github_orgname>`",inline:true},
+        {name:'Login yourself',value: '`git adduser <github_username>`',inline:true},
+        {name:'quote for life',value: '`git quote`',inline:true},
+        {name:'To know more user commands 😎',value: '`git help-user`'},
+        {name:'To know more orgs commands 👯',value: '`git help-org`'},
+        {name:'To know more repo commands 📂',value: '`git help-repo`'},
       )
       .setFooter('Check [git about] to know about me 🤖');
     message.channel.send(embed);

21329c76051ae35e626a1c035999c93200c8f6f2
codetheorem <hrishikeshagarwalv@gmail.com>
2020-06-27 17:50:55 +0530

    update

--- commands/help-org.js
+++ commands/help-org.js
@@ -1,29 +1,33 @@
-
 const Discord = require("discord.js");
 
 module.exports = {
   name: "help-org",
   description: "shows command list",
   execute(message, args) {
-   
-    if(message.guild===null){
+    if (message.guild === null) {
       return message.channel.send("I can't execute that command inside DMs!");
     }
 
     const embed = new Discord.MessageEmbed()
       .setColor("#EFFF00")
       .setTitle("Gitty Org Command list")
-      .setDescription('Gitty is a discord bot to help you with Open source')
+      .setDescription("Gitty is a discord bot to help you with Open source")
       .addFields(
-        {name:"Org Details",value: "`git org-info`",inline: true},
-        {name:"Org Members",value: "`git org-member`",inline: true},
-        {name:"Org repos",value: "`git org-repos`",inline: true},
+        { name: "Org Details", value: "`git org-info`", inline: true },
+        { name: "Org Members", value: "`git org-member`", inline: true },
+        { name: "Org repos", value: "`git org-repos`", inline: true }
       )
-      .addFields(
-        {name:"You need to register your org first to be able to use the above commands!", value:"Run this command to add org `git addorg <org_name>`"}
-    )
-      .setFooter('Check [git about] to know about me 🤖');
+      .addFields({
+        name:
+          "You need to register your org first to be able to use the above commands!",
+        value: "Run this command to add org `git addorg <org_name>`",
+      })
+      .addFields({
+        name: "You can also see other orgs info.",
+        value:
+          "Run this command  `git org-info <github_orgname>`.\n Follow this pattern for other commands.",
+      })
+      .setFooter("Check [git about] to know about me 🤖");
     message.channel.send(embed);
-
   },
 };
--- commands/memberinfo.js
+++ commands/memberinfo.js
@@ -1,7 +1,20 @@
-module.exports={
-    name : 'memberinfo',
-    description : 'Tells user info',
-    execute(message,args){
-        message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+const userinfo = require("../controllers/user-info");
+
+module.exports = {
+  name: "memberinfo",
+  description: "Tells user info",
+  async execute(message, args) {
+    message.channel.send(
+      `Your username: ${message.author.username}\nYour ID: ${message.author.id}`
+    );
+    var username = null;
+    username = await userinfo.execute(message.author.id);
+    if (username) {
+      message.channel.send(`Your Github username is ${username}`);
+    } else {
+      message.channel.send(
+        "You are not registered.Use `git adduser <github_username>`or `git help-user`"
+      );
     }
-}
+  },
+};

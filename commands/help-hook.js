bed2a11130b3aaba6f97c51dee82389b339c5b03
Hrishikesh Agarwal <hrishikeshagarwalv@gmail.com>
2020-06-28 15:14:00 +0530

    Update help-hook.js

--- commands/help-hook.js
+++ commands/help-hook.js
@@ -17,7 +17,7 @@ module.exports = {
       .addFields(
         {name:"Add webhook",value: "`git addwebhook <reponame>`",inline: true},
         {name:"Before using above command give gitty manage webhooks permission.",value: "[Know more](https://bit.ly/31ijYxr)"},
-        {name:"To know more about Gitty's webhook feature.",value: "[Know more](https://bit.ly/31ijYxr)",},
+        {name:"To know more about Gitty's webhook feature.",value: "[Know more](https://bit.ly/38aohvP)",},
       )
       .addFields(
         {name:"You need to register your org first to be able to use the above commands!", value:"Run this command to add org `git addorg <org_name>`"}
